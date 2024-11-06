33392682b940806c8114f4e22ac036f0ee6cb0c3
Simon Bondarenko <bondarenko@vt.edu>
2024-11-06 07:00:37 -0600

    Added tabs. Memorized them

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { memo, useState } from 'react';
+
+const Tab = memo((props: {content: string}) => {
+  const now = performance.now()
+  while (performance.now() - now < 300){}
+  return (
+    <div className="Tab">
+      {props.content}
+    </div>
+  )
+})
+
+const tabNames = ["Tab 1", "Tab 2", "Tab 3"]
+
+
 function App() {
+
+  const [activeTab, setActiveTab] = useState(0)
+  const [inputValue, setInputValue] = useState("")
+
+  const getTabHandler = (tabIdx: number) => {
+    return () => {
+      setActiveTab(tabIdx)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+      <p>Value is: {inputValue}</p>
+      <div className='tab-selector-set'>
+        {tabNames.map((tab, i) => (
+          <button disabled={activeTab === i} onClick={getTabHandler(i)}>{tab}</button>
+        ))}
+      </div>
+
+      <div className='tabs-set'>
+        {tabNames.map((tab, i) => (
+          activeTab === i && <Tab  content={tab} />
+        ))}
+      </div>
     </div>
   );
 }
